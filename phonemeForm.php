c78cd08950ea3f2c7c18ef16677cdb35bd04fc39
YoYoDrif <randashley@rocketmail.com>
2023-09-14 22:33:59 -0400

    add and delete fully functional

--- phonemeForm.php
+++ phonemeForm.php
@@ -1,5 +1,6 @@
 <?php
     session_start();
+    require 'includes/dbh.inc.php';
 ?>
 
 <!DOCTYPE html>
@@ -22,12 +23,9 @@
     </script>
 </head>
 
-
-
 <body class="container" style="background-color: black; margin-top: 20px;">
 
     <?php
-    $language = $_SESSION["language"];
     $effect = NULL;
     $type = NULL;
     $phonemeID = NULL;
@@ -47,7 +45,6 @@
                     <form method="post" action="phonemeSubmit.php">
                         <input name='type' id='type' type='hidden' value='$type'></input>
                         <input name='effect' id='effect' type='hidden' value='$effect'></input>
-                        <input id="phonemeID" type="hidden" value="-1"></input>
                         <label for="place">Place of Articulation:</label>
                         <select name="place" id="place" required="true">
                             <option value="" disabled="true" selected="true">Select One</option>
@@ -102,7 +99,6 @@
                     <form method="post" action="phonemeSubmit.php">
                         <input name='type' id='type' type='hidden' value='$type'></input>
                         <input name='effect' id='effect' type='hidden' value='$effect'></input>
-                        <input id="phonemeID" type="hidden" value="-1"></input>
                         <label for="height">Height:</label>
                         <select name="height" id="height" required="true">
                             <option value="" disabled="true" selected="true">Select One</option>
@@ -151,96 +147,92 @@
             if(isset($_GET["phonemeID"])) {
                 $phonemeID = $_GET["phonemeID"];
             }
+            $tsql = "select * from tblPhonemes where PhonemeID='".$phonemeID."';";
+            $stmt = sqlsrv_query($conn,$tsql);
+            if($stmt == false){
+                echo "Error";
+            }
+            else{
+                $phonemeName = NULL;
+                $phonemeCategory = NULL;
+                //consonant
+                $voicing = NULL;
+                $place = NULL;
+                $manner = NUll;
+                //vowel
+                $height = NULL;
+                $length = NULL;
+                $rounding = NULL;
+                while($obj = sqlsrv_fetch_array($stmt,SQLSRV_FETCH_ASSOC)){
+                    $phonemeName = $obj['PhonemeName'];
+                    $phonemeCategory = $obj['Category'];
 
-            $datas = file_get_contents('json-files/phonemes.json');
-            $phonemes = json_decode($datas, true);//get and decode json
+                    if($phonemeCategory == "C"){
+                        $voicing = $obj['Voicing'];
+                        $place = $obj['Place'];
+                        $manner = $obj['Manner'];
 
-            $count= count($phonemes);
-            if($count > 0){
-                for ($i=0; $i < $count; $i++) {
-                    $phonemeName = NULL;
-                    $phonemeCategory = NULL;
-                    //consonant
-                    $voicing = NULL;
-                    $place = NULL;
-                    $manner = NUll;
-                    //vowel
-                    $height = NULL;
-                    $length = NULL;
-                    $rounding = NULL;
+                        echo <<<GFG
+                            <h1 class='display-3 text-primary'>You are about to delete the following Consonant</h1>
+                            <div class="rounded-3 fs-4 text-white" style="background-color: #2c3034; padding: 20px;">
+                            <form method="post" action="phonemeSubmit.php">
+                                <input name='phonemeID' id='type' type='hidden' value='$phonemeID'></input>
+                                <input name='effect' id='effect' type='hidden' value='$effect'></input>
+                                <label>Name: $phonemeName</label>
 
-                    if($phonemes[$i]["phonemeID"] == $phonemeID) {
-                        $language = $phonemes[$i]["language"];
-                        if($phonemes[$i]["category"] == "C") {
-                            $phonemeCategory = $phonemes[$i]["category"];
-                            $phonemeName = $phonemes[$i]["voicing"] . " " . $phonemes[$i]["place"] . " " . $phonemes[$i]["manner"];
-                            $voicing = $phonemes[$i]["voicing"];
-                            $place = $phonemes[$i]["place"];
-                            $manner = $phonemes[$i]["manner"];
-                        }else if($phonemes[$i]["category"] == "V") {
-                            $phonemeCategory = $phonemes[$i]["category"];
-                            $phonemeName = $phonemes[$i]["height"] . " " . $phonemes[$i]["length"] . " " . $phonemes[$i]["rounding"] . " vowel";
-                            $height = $phonemes[$i]["height"];
-                            $length = $phonemes[$i]["length"];
-                            $rounding = $phonemes[$i]["rounding"];
-                        }
+                                </br>
 
-                        if($phonemes[$i]["category"] == "C") {
-                            echo <<<GFG
-                                <h1 class='display-3 text-primary'>Would you like to delete the following Consonant?</h1>
-                                <div class="rounded-3 fs-4 text-white" style="background-color: #2c3034; padding: 20px;">
-                                <form method="post" action="phonemeSubmit.php">
-                                    <input name='type' id='type' type='hidden' value='$phonemeCategory'></input>
-                                    <input name='effect' id='effect' type='hidden' value='$effect'></input>
-                                    <input name='phonemeID' id="phonemeID" type="hidden" value='$phonemeID'></input>
+                                <label>Place: $place</label>
 
-                                    <input id="place" type="hidden" value='$place'></input>
-                                    <input id="manner" type="hidden" value='$manner'></input>
-                                    <input id="voicing" type="hidden" value='$voicing'></input>
+                                </br>
 
-                                    <h1 class='display-5 text-primary'>Place of Articulation: $place</h1>
-                                    </br>
-                                    <h1 class='display-5 text-primary'>Manner of Articulation: $manner</h1>
-                                    </br>
-                                    <h1 class='display-5 text-primary'>Voicing: $voicing</h1>
-                                    </br>
-                    
-                                    <input type="submit" name="submit" value="Submit">
-                                </form>
-                                </div>
-                            GFG;
-                        }
-                        if($phonemes[$i]["category"] == "V") {
-                            echo <<<GFG
-                                <h1 class='display-3 text-primary'>Would you like to delete the following Vowel?</h1>
-                                <div class="rounded-3 fs-4 text-white" style="background-color: #2c3034; padding: 20px;">
-                                <form method="post" action="phonemeSubmit.php">
-                                    <input name='type' id='type' type='hidden' value='$phonemeCategory'></input>
-                                    <input name='effect' id='effect' type='hidden' value='$effect'></input>
-                                    <input name='phonemeID' id="phonemeID" type="hidden" value='$phonemeID'></input>
+                                <label>Manner: $manner</label>
 
-                                    <input id="height" type="hidden" value='$height'></input>
-                                    <input id="length" type="hidden" value='$length'></input>
-                                    <input id="rounding" type="hidden" value='$rounding'></input>
+                                </br>
 
-                                    <h1 class='display-5 text-primary'>Height: $height</h1>
-                                    </br>
-                                    <h1 class='display-5 text-primary'>Length: $length</h1>
-                                    </br>
-                                    <h1 class='display-5 text-primary'>Rounding: $rounding</h1>
-                                    </br>
-                    
-                                    <input type="submit" name="submit" value="Submit">
-                                </form>
-                                </div>
-                            GFG;
-                        }
+                                <label>Is it Voiced?: $voicing</label>
+
+                                </br>
+
+                                <input type="submit" name="submit" value="Submit">
+                            </form>
+                            </div>
+                        GFG;
+                    }
+                    else if($phonemeCategory == "V"){
+                        $height = $obj['Height'];
+                        $length = $obj['VLength'];
+                        $rounding = $obj['Rounding'];
+
+                        echo <<<GFG
+                            <h1 class='display-3 text-primary'>You are about to delete the following Vowel</h1>
+                            <div class="rounded-3 fs-4 text-white" style="background-color: #2c3034; padding: 20px;">
+                            <form method="post" action="phonemeSubmit.php">
+                                <input name='phonemeID' id='type' type='hidden' value='$phonemeID'></input>
+                                <input name='effect' id='effect' type='hidden' value='$effect'></input>
+                                <label>Name: $phonemeName</label>
+
+                                </br>
+
+                                <label>Height: $height</label>
+
+                                </br>
+
+                                <label>Length: $length</label>
+
+                                </br>
+
+                                <label>Is it Rounded?: $rounding</label>
+
+                                </br>
+
+                                <input type="submit" name="submit" value="Submit">
+                            </form>
+                            </div>
+                        GFG;
                     }
                 }
             }
-        }
-        else {
-
         }
     }
     ?>
--- phonemeSubmit.php
+++ phonemeSubmit.php
@@ -1,10 +1,6 @@
 <?php
     session_start();
-
-    $datas = file_get_contents('json-files/phonemes.json');
-    $phonemes = json_decode($datas, true);//get and decode json
-
-    $message = "test";
+    require 'includes/dbh.inc.php';
 
     $effect = $_POST["effect"];
     $phonemeID = NULL;
@@ -18,56 +14,48 @@
     $height = NULL;
     $length = NULL;
     $rounding = NULL;
+    if(isset($_SESSION["languageID"])) {
+        $languageID = $_SESSION["languageID"];
+    }
+    else {
+        die();
+    }
 
     if($_POST["effect"] == "add") {
         if($_POST["type"] == "C") {
-            $message = "You added a " . $_POST["voicing"] ." ". $_POST["place"] ." ". $_POST["manner"];
-            
             $phonemeName = $_POST["voicing"] . " " . $_POST["place"] . " " . $_POST["manner"];
             $phonemeCategory = $_POST["type"];
             $place = $_POST["place"];
             $manner = $_POST["manner"];
             $voicing = $_POST["voicing"];
     
-            $newID = uniqid(); 
+            $tsql = "insert into tblPhonemes (PhonemeName, Category, Place, Manner, Voicing, LanguageID) values (?, ?, ?, ?, ?, ?);";
+            $params = array($phonemeName, $phonemeCategory, $place, $manner, $voicing, $languageID);
+
+            $stmt = sqlsrv_query($conn,$tsql,$params);
     
-            $phoneme = array(
-                "phonemeID" => $newID,
-                "name" => $phonemeName,
-                "language"=> $_SESSION["language"],
-                "category"=> $phonemeCategory,
-                "place"=> $place,
-                "manner"=> $manner,
-                "voicing"=> $voicing
-            );
-    
-            array_push($phonemes, $phoneme);
+            if( $stmt === false ) {
+                die( print_r( sqlsrv_errors(), true));
+           }
         }
         else if($_POST["type"] == "V") {
-            $message = "You added a " . $_POST["height"] ." ". $_POST["length"] ." ". $_POST["rounding"] ." vowel";
-    
             $phonemeName = $_POST["height"] . " " . $_POST["length"] . " " . $_POST["rounding"] . " vowel";
             $phonemeCategory = $_POST["type"];
             $height = $_POST["height"];
             $length = $_POST["length"];
             $rounding = $_POST["rounding"];
     
-            $newID = uniqid(); 
+            $tsql = "insert into tblPhonemes (PhonemeName, Category, Height, VLength, Rounding, LanguageID) values (?, ?, ?, ?, ?, ?);";
+            $params = array($phonemeName, $phonemeCategory, $height, $length, $rounding, $languageID);
+
+            $stmt = sqlsrv_query($conn,$tsql,$params);
     
-            $phoneme = array(
-                "phonemeID" => $newID,
-                "name" => $phonemeName,
-                "language"=> $_SESSION["language"],
-                "category"=> $phonemeCategory,
-                "height"=> $height,
-                "length"=> $length,
-                "rounding"=> $rounding
-            );
-    
-            array_push($phonemes, $phoneme);
+            if( $stmt === false ) {
+                die( print_r( sqlsrv_errors(), true));
+            }
         }
         else {
-            $message = "Error";
+            //test
         }
     }
     else if($_POST["effect"] == "edit") {
@@ -75,146 +63,18 @@
     }
     else if($_POST["effect"] == "delete") {
         $phonemeID = $_POST["phonemeID"];
-        if($_POST["type"] == "C") {
-            //$message = "You deleted a " . $_POST["voicing"] ." ". $_POST["place"] ." ". $_POST["manner"];
-            
+        $tsql = "delete from tblPhonemes where PhonemeID = ?;";
+        $params = array($phonemeID);
 
-            foreach($phonemes as $key=>$phoneme) {
-                if($phoneme['phonemeID'] == $phonemeID) {
-                    unset($phonemes[$key]);
-                }
-            }
-        }
-        else if($_POST["type"] == "V") {
-            $message = "You added a " . $_POST["height"] ." ". $_POST["length"] ." ". $_POST["rounding"] ." vowel";
-    
-            $phonemeName = $_POST["height"] . " " . $_POST["length"] . " " . $_POST["rounding"] . " vowel";
-            $phonemeCategory = $_POST["type"];
-            $height = $_POST["height"];
-            $length = $_POST["length"];
-            $rounding = $_POST["rounding"];
-    
-            $newID = uniqid(); 
-    
-            $phoneme = array(
-                "phonemeID" => $newID,
-                "name" => $phonemeName,
-                "language"=> $_SESSION["language"],
-                "category"=> $phonemeCategory,
-                "height"=> $height,
-                "length"=> $length,
-                "rounding"=> $rounding
-            );
-    
-            array_push($phonemes, $phoneme);
-        }
-        else {
-            $message = "Error";
+        $stmt = sqlsrv_query($conn,$tsql,$params);
+
+        if( $stmt === false ) {
+            die( print_r( sqlsrv_errors(), true));
         }
     }
     else {
-        $message = "Error";
+        //test
     }
 
-    $phonemes = json_encode($phonemes);
-
-    file_put_contents('json-files/phonemes.json', $phonemes);
-
-    header("Location:languageOverview.php");
-    
-    // if(isset($_POST["effect"])) {
-    //     $effect = $_POST["effect"];
-
-    //     if($effect == "add"){
-    //         if($_POST["type"] == "C") {
-    //             $message = "You added a " . $_POST["voicing"] ." ". $_POST["place"] ." ". $_POST["manner"];
-                
-    //             $phonemeName = $_POST["voicing"] . " " . $_POST["place"] . " " . $_POST["manner"];
-    //             $phonemeCategory = $_POST["type"];
-    //             $place = $_POST["place"];
-    //             $manner = $_POST["manner"];
-    //             $voicing = $_POST["voicing"];
-        
-    //             $newID = uniqid(); 
-        
-    //             $phoneme = array(
-    //                 "phonemeID" => $newID,
-    //                 "name" => $phonemeName,
-    //                 "language"=> $_SESSION["language"],
-    //                 "category"=> $phonemeCategory,
-    //                 "place"=> $place,
-    //                 "manner"=> $manner,
-    //                 "voicing"=> $voicing
-    //             );
-        
-    //             array_push($phonemes, $phoneme);
-    //         }
-    //         else if($_POST["type"] == "V") {
-    //             $message = "You added a " . $_POST["height"] ." ". $_POST["length"] ." ". $_POST["rounding"] ." vowel";
-        
-    //             $phonemeName = $_POST["height"] . " " . $_POST["length"] . " " . $_POST["rounding"] . " vowel";
-    //             $phonemeCategory = $_POST["type"];
-    //             $height = $_POST["height"];
-    //             $length = $_POST["length"];
-    //             $rounding = $_POST["rounding"];
-        
-    //             $newID = uniqid(); 
-        
-    //             $phoneme = array(
-    //                 "phonemeID" => $newID,
-    //                 "name" => $phonemeName,
-    //                 "language"=> $_SESSION["language"],
-    //                 "category"=> $phonemeCategory,
-    //                 "height"=> $height,
-    //                 "length"=> $length,
-    //                 "rounding"=> $rounding
-    //             );
-        
-    //             array_push($phonemes, $phoneme);
-    //         }
-    //         else {
-    //             $message = "Error";
-    //         }
-        
-    //         $phonemes = json_encode($phonemes);
-        
-    //         file_put_contents('json-files/phonemes.json', $phonemes);
-        
-    //         header("Location:languageOverview.php");
-    //     }
-    //     elseif($effect == "edit") {
-    //         //test
-    //     }
-    //     elseif($effect == "delete") {
-    //         if($_POST["type"] == "C") {
-    //             $message = "You deleted a " . $_POST["voicing"] ." ". $_POST["place"] ." ". $_POST["manner"];
-                
-    //             //test
-        
-    //             array_push($phonemes, $phoneme);
-    //         }
-    //         else if($_POST["type"] == "V") {
-    //             $message = "You deleted a " . $_POST["height"] ." ". $_POST["length"] ." ". $_POST["rounding"] ." vowel";
-        
-    //             //test
-        
-    //             array_push($phonemes, $phoneme);
-    //         }
-    //         else {
-    //             $message = "Error";
-    //         }
-
-    //         //echo "<html><h1>" . "test" . "</h1></html>";
-        
-    //         $phonemes = json_encode($phonemes);
-        
-    //         file_put_contents('json-files/phonemes.json', $phonemes);
-        
-    //         header("Location:languageOverview.php");
-    //     }
-    //     else {
-    //         $message = "Error";
-    //     }
-    // }
-
+    header("Location:phonemeOverview.php");
 ?>
